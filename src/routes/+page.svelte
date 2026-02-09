4084f73d82ee42acfb8de1435c1fa20c18cd9017
Le-vry <elliotlidman@gmail.com>
2026-02-09 13:05:41 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
 		padding: 2rem;
 		display: flex;
 		flex-direction: column;
-        justify-content: center;
+        justify-content: top;
         align-items: center;
 		gap: 2rem;
 		overflow: hidden;
