ebd664e09ebc3b39fbe570846b0b78f5828ea0f6
Le-vry <elliotlidman@gmail.com>
2026-02-10 09:07:14 +0100

    Progress?

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
-    url: "postgresql://charactertrackerdb_user:A1ov2hMZuxVp25aoKaJEG50h3MUc6DoF@dpg-d64tl8ngi27c73bal8tg-a.frankfurt-postgres.render.com/charactertrackerdb",
+    url: "postgresql://charactertrackerdb_user:A1ov2hMZuxVp25aoKaJEG50h3MUc6DoF@dpg-d64tl8ngi27c73bal8tg-a/charactertrackerdb",
   },
 });
