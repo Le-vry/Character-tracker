7a323214472aa1e94c6d0f9ea2af67642fd38033
Le-vry <elliotlidman@gmail.com>
2026-02-10 08:52:49 +0100

    Progress!

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,20 +1,4 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
 
-// Singleton pattern to prevent multiple instances during dev hot reload
-let prisma: PrismaClient;
-
-if (import.meta.env.DEV) {
-  // @ts-ignore
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-} else {
-  prisma = new PrismaClient();
-}
-
-export { prisma };
+export const prisma = new PrismaClient();
