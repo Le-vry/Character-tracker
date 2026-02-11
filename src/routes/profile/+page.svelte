98ec41a41fc729ae10737e117a5fafa1c6898de6
Le-vry <elliotlidman@gmail.com>
2026-02-11 13:34:20 +0100

    File uppload

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,9 +1,24 @@
-<script>
-	import { enhance } from "$app/forms";
+<script lang="ts">
+  import { enhance } from "$app/forms";
+
+  let previewUrl: string | null = null;
+
+  function handleFileChange(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    if (files && files[0]) {
+      previewUrl = URL.createObjectURL(files[0]);
+    } else {
+      previewUrl = null;
+    }
+  }
 
 </script>
 
 <form method="POST" action="?/upload" enctype="multipart/form-data" use:enhance>
-  <input type="image" name="image" accept=".png/.jpg/.jpeg" alt="profilepicture" />
+  <input type="file" name="image" accept="image/*" on:change={handleFileChange} />
   <button type="submit">Upload Image</button>
 </form>
+
+{#if previewUrl}
+  <img src={previewUrl} alt="Preview" style="max-width:200px;max-height:200px" />
+{/if}
