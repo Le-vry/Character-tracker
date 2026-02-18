a9f744a7f64e35022487a9780af3f45bf0e2ce6f
Le-vry <elliotlidman@gmail.com>
2026-02-18 12:25:44 +0100

    Added cryptation

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,6 +2,21 @@ import { fail, redirect } from '@sveltejs/kit';
 import { prisma } from '$lib';
 import type { Actions } from './$types';
 
+import * as crypto from "node:crypto";
+
+// Function to generate a new salt and hash a password
+function hashPassword(password: string): { salt: string; hash: string } {
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+  return { salt, hash };
+}
+
+// Function to validate a password against a stored salt and hash
+function validatePassword(inputPassword: string, storedSalt: string, storedHash: string): boolean {
+  const hash = crypto.pbkdf2Sync(inputPassword, storedSalt, 10000, 64, 'sha512').toString('hex');
+  return storedHash === hash;
+}
+
 export const actions: Actions = {
 	login: async ({ request, cookies }) => {
 		const data = await request.formData();
@@ -20,6 +35,12 @@ export const actions: Actions = {
 			return fail(400, { error: 'Invalid credentials' });
 		}
 
+		const isValidPassword = validatePassword(password, user.salt, user.hash);
+
+		if (!isValidPassword) {
+			return fail(400, { error: 'Invalid credentials' });
+		}
+		
 		cookies.set('userId', user.id, {
 			path: '/',
 			httpOnly: true,
@@ -38,6 +59,14 @@ export const actions: Actions = {
 		if (!username || !password || typeof username !== 'string' || typeof password !== 'string') {
 			return fail(400, { error: 'Missing username or password' });
 		}
+			
+		if (username.length < 3) {
+			return fail(400, { error: 'Username must contain atleast 3 characters' });
+		}
+			
+		if (password.length < 6) {
+			return fail(400, { error: 'Password must contain atleast 6 characters' });
+		}
 
 		const existingUser = await prisma.user.findUnique({
 			where: { username }
@@ -47,13 +76,16 @@ export const actions: Actions = {
 			return fail(400, { error: 'User already exists' });
 		}
 
+		const { salt, hash } = hashPassword(password);
+
 		// helper to get a random integer between min and max (inclusive)
 		const randInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 		const newUser = await prisma.user.create({
 			data: {
 				username,
-				password,
+				salt: salt,
+        		hash: hash,
 				character: {
 					create: {
 						gamesPlayed: randInt(5, 100),
